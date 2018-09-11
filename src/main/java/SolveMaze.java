d142b8f68d1df8fcad2179253b59d76eef449cc3
yunfeim2 <42976501+yunfeim2@users.noreply.github.com>
2018-09-12 01:23:59 +0800

    sdas

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,13 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+           while (!maze.isFinished()) {
+               maze.turnLeft();
+               while (!maze.canMove()) {
+                   maze.turnRight();
+               }
+               maze.move();
+           }
         }
 
         if (maze.isFinished()) {
